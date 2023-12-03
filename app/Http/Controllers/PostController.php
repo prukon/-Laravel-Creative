71bd8b4f111bb8377d30422811d6ffba2381d0d1
prukon <prukon@gmail.com>
2023-12-03 23:03:37 +0300

    fix PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,10 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Category;
 use App\Models\Post;
-use App\Models\PostTag;
-use App\Models\Tag;
 use App\Models\Student;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
@@ -13,127 +10,93 @@ use Illuminate\Routing\Controller;
 
 class PostController extends Controller
 {
-    public function index()
-    {
+    public function index() {
+
+//Получить запись по id
+        $onePost = Post::find(1);
+
+//        Получить все записи
         $allPosts = Post::all();
+
+//Получить записи по условию
+        $somePost = Post::where('is_published', 1)->get();
+
         return view("post.index", compact("allPosts")); //означает, что мы обращаемся к папке post, в которой файл index.blade.php
 
     }
 
+    public function test()
+    {
+        dump("123");
+    }
+
+
 
 //   create -> store
 //метод для формирования страницы создания чего-либо
-    public function create()
-    {
-        $categories = Category::all();
-        $tags = Tag::all();
+    public function create() {
         $allPosts = post::all();
-        return view("post.create", compact("allPosts", "categories", "tags"));
+//        dump("123");
+
+        return view("post.create", compact("allPosts"));
     }
 
-    //   create -> store
 //  метод для обработки отправленных данных
-    public function store()
-    {
+    public function store() {
 //        в дата указываем какие ключи мы ждем с формы
         $data = request()->validate([
-            "title" => "required|string",
+            "title" => "string",
             "content" => "string",
             "image" => "string",
             "likes" => "integer",
-            "category_id" => "",
-            "tags" => "",
         ]);
-        $tags = $data['tags'];
-        unset($data['tags']);
-
-//      dd  ($tags, $data);
-
-
-//        не проф подход. В нем добалвется в БД сведения о создании и изменении записи
-//        $post = Post::create($data);
-//        foreach ($tags as $tag) {
-//            //не будет создавать дубль запись в БД, если запись уже существует
-//            PostTag::firstOrcreate([
-//                'tag_id' => $tag,
-//                'post_id' => $post->id,
-//            ]);
-//        }
-
-//        проф подход. В нем  не добалвется в БД сведения о создании и изменении записи
-        $post = Post::create($data);
-        $post->tags()->attach($tags);
-
-
-
-
-        //        Post::create($data); //метод create добавляет в БД массив data
+//      dd  ($data);
+        Post::create($data); //метод create добавляет в БД массив data
         return redirect()->route('post.index');
 
     }
 
 //   edit -> update
-    public function edit(Post $post)
-    {
-//        dump($post->title);
-//        $post = Post::all();
-        $categories = Category::all();
-        $tags = Tag::all();
-        return view('post.edit', compact('post', 'categories', 'tags'));
+    public function edit(Post $post) {
+        dump($post->title);
+        return view('post.edit', compact('post'));
     }
 
-    //   edit -> update
-    public function update(Post $post)
-    {
+    public function update(Post $post) {
 
         $data = request()->validate([
             "title" => "string",
             "content" => "string",
             "image" => "string",
             "likes" => "integer",
-            "category_id" => "",
-            "tags" => "",
         ]);
 //      dd  ($data);
-
-        $tags = $data['tags'];
-        unset($data['tags']);
-
         $post->update($data);
-        //метод сначала удаляет запись, потом добавляет их в бд
-        $post->tags()->sync($tags);
-
-
 //        Post::create($data); //метод create добавляет в БД массив data
         return redirect()->route('post.show', $post->id);
 //        dump("1111");
 
     }
 
-    public function show(Post $post)
-    {
+    public function show(Post $post) {
         return view('post.show', compact("post"));
     }
 
-    public function destroy(Post $post)
-    {
+    public function destroy(Post $post) {
         $post->delete();
         return redirect()->route('post.index');
     }
 
     //Доп методы
 
-    public function contacts()
-    {
+    public function contacts(){
         $allPosts = post::all();
         return view("contacts", compact("allPosts"));
     }
 
-
-    public function delete()
-    {
+    public function delete() {
         $post = Post::find(5);
-        $postAll = Post::all();
+        $postAll=Post::all();
         dump($post);
         dump($postAll);
 //            $post->delete();
@@ -141,14 +104,13 @@ class PostController extends Controller
 
 
         //восстановление данных
-        $post = Post::withTrashed()->find(2);
+        $post=Post::withTrashed()->find(2);
         $post->restore();
         dump("restored");
     }
 
     //Если запись уже есть, то ничего не делает. Если записи нет - создает ее.
-    public function firstOrCreate()
-    {
+    public function firstOrCreate() {
         $post = Post::find(1);
         dump($post->title);
 
@@ -176,8 +138,7 @@ class PostController extends Controller
     }
 
     //Если запись уже есть, то обновляет ее. Если записи нет - создает ее.
-    public function updateOrCreate()
-    {
+    public function updateOrCreate() {
         $post = Post::find(1);
         dump($post->title);
 
